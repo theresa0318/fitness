219a59ee404890b1b47cfdfeae95981fa3961e41
jessica-ma3 <jessica@github.no.reply.com>
2021-10-29 00:13:04 +1100

    add more attributes

--- app/src/main/java/com/example/fitsoc/data/model/RegisteredUser.java
+++ app/src/main/java/com/example/fitsoc/data/model/RegisteredUser.java
@@ -2,23 +2,48 @@ package com.example.fitsoc.data.model;
 
 public class RegisteredUser {
     private String userId;
-    private String password;
+    //private String password;
+    private String gender;
+    private int age;
+    private int height;
+    private int weight;
+
+
 
     public RegisteredUser() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public RegisteredUser(String userId, String password) {
+    public RegisteredUser(String userId) {
         this.userId = userId;
-        this.password = password;
     }
 
     public String getUserId() {
         return userId;
     }
 
-    public String getPassword() {
-        return password;
+    public String getGender() {
+        return gender;
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setGender(String gender) { this.gender = gender;}
+
+    public void setAge(int age) { this.age = age;}
+
+    public void setHeight(int height) { this.height = height;}
+
+    public void setWeight(int weight) { this.weight = weight;}
 
 }
