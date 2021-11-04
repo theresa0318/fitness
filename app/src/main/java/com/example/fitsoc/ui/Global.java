c7a564e3dbfc42e8583fe2737b5d69a29591f818
jessica-ma3 <jessica19991108@hotmail.com>
2021-11-05 06:43:09 +1100

    add user

--- app/src/main/java/com/example/fitsoc/ui/Global.java
+++ app/src/main/java/com/example/fitsoc/ui/Global.java
@@ -2,9 +2,12 @@ package com.example.fitsoc.ui;
 
 import android.app.Application;
 
+import com.google.firebase.auth.FirebaseUser;
+
 public class Global extends Application {
     private static String userID = null;
     private static String password = null;
+    private static FirebaseUser user;
 
     public static String getUserID() {
         return userID;
@@ -20,6 +23,14 @@ public class Global extends Application {
 
     public static void setPassword(String password) {
         Global.password = password;
+    }
+
+    public static FirebaseUser getUser() {
+        return user;
+    }
+
+    public static void setUser(FirebaseUser user) {
+        Global.user = user;
     }
 
 }
