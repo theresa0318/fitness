c12ba2601ebc58789448180ec9006d3803c122ca
aabbaq <43639882+aabbaq@users.noreply.github.com>
2021-11-04 17:23:15 +0800

    Merge Bug Fixed

--- app/src/main/java/com/example/fitsoc/NavigationActivity.java
+++ app/src/main/java/com/example/fitsoc/NavigationActivity.java
@@ -94,7 +94,7 @@ public class NavigationActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
       
-        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_run, R.id.nav_community, R.id.nav_calorie, R.id.nav_history, R.id.nav_profile, R.id.nav_settings, R.id.nav_logout)
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_run, R.id.nav_community, R.id.nav_task, R.id.nav_history, R.id.nav_profile, R.id.nav_settings, R.id.nav_logout)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation);
